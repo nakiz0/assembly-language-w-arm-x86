f9b0cc28d0bb43b720dbb2413ae60e1a7850526a
Amrit <amritxtamu132@gmail.com>
2026-06-08 21:00:14 +0530

    add

--- data.asm
+++ data.asm
@@ -1,10 +1,10 @@
 section .data
-    num dd 5    # <-- define doubleword 
+    num dd 5    ;# <-- define doubleword 
 
 section .text
 global _start
 
 _start:
     mov eax,1
-    mov ebx,[num]   # <-- [] signifies to go to the address and store the value in the register
+    mov ebx,[num]   ;# <-- [] signifies to go to the address and store the value in the register
     int 80h        
