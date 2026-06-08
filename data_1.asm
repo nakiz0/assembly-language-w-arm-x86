344a78b5ae2b23bc2a9f284bb10777cf76d26a40
Amrit <amritxtamu132@gmail.com>
2026-06-08 18:02:12 +0530

    data_1

--- data_1.asm
+++ data_1.asm
@@ -1,10 +1,12 @@
 section .data 
+    num db 1
+    nub2 db 2
 
 section .text
-
 global _start
-start:
-    move bx,[num]
+
+_start:
+    move ebx,[num]
     mov ecx,[num2]
     mov eax,1
     int 80h
