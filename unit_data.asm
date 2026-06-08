046d9d9f20827bd69d15ad3b05e2b4868d56c30f
Amrit <amritxtamu132@gmail.com>
2026-06-08 19:56:35 +0530

    data uninitialized

--- unit_data.asm
+++ unit_data.asm
@@ -1,9 +1,13 @@
 section .bss
-    num resb 3
+    num resb 3             ;#<-- reserved data 
 
 section .data
-
+    num2 db 3 dup(2)          ;#<-- duplicating 2 thrice
 section _text
 global _start
 start:
-    
+    mov bl,1
+    mov bl,[num2]
+    mov [num],bl
+    mov [num+1],bl
+    mov [num+2],bl
