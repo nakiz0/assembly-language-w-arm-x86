39f92d58144f0186eb1437d4f4f845ad45929b5d
Amrit <amritxtamu132@gmail.com>
2026-06-09 13:09:59 +0530

    looping

---
+++ loops.asm
@@ -0,0 +1,22 @@
+section .data
+    list db 1,2,3,4
+
+section .text
+global _start
+
+_start:
+    mov eax, 0
+    mov cl, 0
+
+loop:
+    mov bl, [list+eax]
+    add cl,bl
+    inc eax
+    cmp eax,4
+    je end 
+    jmp loop
+
+end:
+    mov eax,1
+    mov ebx,1
+    int 80h
